5d69d0f9340eef533715362710ca5dddf1302054
Maxim <msirotkin95@gmail.com>
2024-04-30 10:25:34 +0300

    Step 2

---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,32 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+
+
+public class App {
+
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.valueOf(port);
+    }
+
+    public static Javalin getApp() {
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+        });
+
+        app.get("/", ctx -> ctx.result("Hello World"));
+        return app;
+    }
+
+    public static void main(String[] args) {
+        var app = getApp();
+
+        app.start(getPort());
+    }
+
+}
