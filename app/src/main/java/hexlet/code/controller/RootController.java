ed107ed6c1cc32e17d28108334008f1edd4bdeca
Maxim <msirotkin95@gmail.com>
2024-06-06 11:36:58 +0300

    fix2

--- app/src/main/java/hexlet/code/controller/RootController.java
+++ app/src/main/java/hexlet/code/controller/RootController.java
@@ -1,13 +1,13 @@
 package hexlet.code.controller;
 
-import hexlet.code.dto.BasePage;
+//import hexlet.code.dto.BasePage;
 import io.javalin.http.Context;
 
-import static io.javalin.rendering.template.TemplateUtil.model;
+//import static io.javalin.rendering.template.TemplateUtil.model;
 
 public class RootController {
     public static void index(Context ctx) {
-            ctx.render("index.jte");
+        ctx.render("index.jte");
       /*  var page = new BasePage();
         page.setFlash(ctx.consumeSessionAttribute("flash"));
         page.setFlashType(ctx.consumeSessionAttribute("flash-type"));
