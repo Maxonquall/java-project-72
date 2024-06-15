075abe1e08a90800f55594e5949f79148c99db0b
Maxim <msirotkin95@gmail.com>
2024-06-15 20:21:07 +0300

    fix2

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -79,7 +79,8 @@ public class App {
         BaseRepository.dataSource = dataSource;
 
         var app = Javalin.create(config -> {
-            config.plugins.enableDevLogging();
+            config.bundledPlugins.enableDevLogging();
+            //  config.fileRenderer(new JavalinJte());
         });
 
         JavalinJte.init(createTemplateEngine());
